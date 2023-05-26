0eb6a63c74ab4e0002b5958f56a36fba67f6c289
Girish1369 <girishkatkamwar.1369@gmail.com>
2023-05-26 22:07:28 +0530

    division occur at local

---
+++ src/com/demo/divisiononworkplace.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class divisiononworkplace {
+
+	public static void main(String[] args) {
+		// division by push operation
+		int a=50;
+		int b=10;
+		System.out.println("division="+(a/b));
+
+	}
+
+}
